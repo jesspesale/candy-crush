2e67c77e0f8c0b2014531f1d73bfcbdff69dc79d
jesspesale <jess_pesale@aol.com>
2022-01-23 18:37:58 -0500

    got createBoard function and useState/useEffect working

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+
 const width = 8
 
 const candyColors = [
@@ -9,9 +11,30 @@ const candyColors = [
   "yellow"
 ]
 
-function App() {
+const  App = () => {
+
+  const [currentColorArangement, setCurrentColorArangement] = useState([])
+
+  const createBoard = () => {
+    const randomeColorArangement = []
+    for(let i = 0; i < width * width; i++){
+      const randomNumber = Math.floor(Math.random() * candyColors.length)
+      // floor for rounded up on numbers, choose a random number that is 1-8
+      const randomColor = candyColors[randomNumber]
+        randomeColorArangement.push(randomColor)
+    }
+    setCurrentColorArangement(randomeColorArangement)
+    console.log(currentColorArangement)
+  }
+
+  useEffect(() => {
+    createBoard()
+  }, [])
+  // the empty array makes it so the effect will only run once after state changes
+
   return (
     <div className="App">
+      Hi
     </div>
   );
 }
