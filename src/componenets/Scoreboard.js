7bb9931e284415f1f9790667414b2b3bd6d05b56
jesspesale <jess_pesale@aol.com>
2022-01-23 22:51:36 -0500

    added styling for Scoreboard

--- src/componenets/Scoreboard.js
+++ src/componenets/Scoreboard.js
@@ -2,11 +2,13 @@
 const Scoreboard = ({score}) => {
 
     return (
-        <div className="score-board">
-            <h2>
-                {score}
-            </h2>
+        <div className="score-board-box">
+            <div className="score-board">
+                <h2>
+                    Your Score: {score}
+                </h2>
 
+            </div>
         </div>
     )
 }
