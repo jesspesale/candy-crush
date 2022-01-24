6768f902590117d6d3662232074c036fd8968d76
jesspesale <jess_pesale@aol.com>
2022-01-23 19:25:14 -0500

    set up checkforColumnOfThree function and its useEffect

--- src/App.js
+++ src/App.js
@@ -15,8 +15,19 @@ const  App = () => {
 
   const [currentColorArrangement, setCurrentColorArrangement] = useState([])
 
-  const createBoard = () => {
+  const checkForColumnOfThree = () => {
+    for(let i = 0; i < 47; i++){
+      const columnOfThree = [i, i +width, i +width * 2]
+      const decidedColor = currentColorArrangement[i]
 
+      if(columnOfThree.every(square => currentColorArrangement[square] === decidedColor)){
+        // checking if 3 columns in a row are the same color as the first one checked
+          columnOfThree.forEach(square => currentColorArrangement[square] = " ")
+      }
+    }
+  }
+
+  const createBoard = () => {
     const randomColorArrangement = []
 
     for(let i = 0; i < width * width; i++){
@@ -34,6 +45,15 @@ const  App = () => {
   }, [])
   // the empty array makes it so the effect will only run once after state changes
 
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfThree()
+      setCurrentColorArrangement([...currentColorArrangement])
+    }, 100)
+    // run a new interval every 100ms
+    return () => clearInterval(timer)
+  }, [checkForColumnOfThree], currentColorArrangement)
+
 
   console.log(currentColorArrangement)
 
@@ -45,6 +65,7 @@ const  App = () => {
           <img
             key={index}
             style={{backgroundColor: candyColor}}
+            alt={candyColor}
           />
         ))}
 
