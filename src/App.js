044a7329a66f24a3ce513589b2f5c65778a163bc
jesspesale <jess_pesale@aol.com>
2022-01-31 20:03:03 -0500

    moved checkWin function to App.js

--- src/App.js
+++ src/App.js
@@ -8,17 +8,53 @@ const  App = () => {
   const [turns, setTurns] = useState(0)
   const [score, setScore] = useState(0)
 
-  console.log(score)
+
+      const checkWin = () => {
+        if(score >= 50 && turns < 10){
+            return (
+                <div>
+                    <h2>You won with a score of: {score}</h2>
+                </div>
+            )
+        } else if (score < 50 && turns >= 10){
+            return (
+                <div className="display_score">
+                    <h2>Sorry, you lost ☹</h2> 
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                  <h1 className="game_name">Welcome to M&M Crush!</h1>
+                  <div className="how_to_play">
+                      <h2>You must score at least 50 points in under 15 moves to win</h2>
+                      <h3></h3>
+                  </div>
+                  <Game 
+                    turns={turns}      setScore={setScore}
+                    score={score}      setTurns={setTurns}
+                  /> 
+                  <Scoreboard score={score} turns={turns}/>
+                </div>
+                )
+            }
+    }
 
   return (
     <div className="app">
-      <h1 className="game_name">Welcome to M&M Crush!</h1>
+      {checkWin()}
+      {/* <h1 className="game_name">Welcome to M&M Crush!</h1>
       <div className="how_to_play">
           <h2>You must score at least 50 points in under 15 moves to win</h2>
           <h3></h3>
       </div>
-      <Game turns={turns} score={score} setScore={setScore} setTurns={setTurns}/> 
-       <Scoreboard score={score} turns={turns}/>
+      <Game 
+        turns={turns} 
+        score={score} 
+        setScore={setScore} 
+        setTurns={setTurns}
+      /> 
+       <Scoreboard score={score} turns={turns}/> */}
     </div>
   );
 }
